28169536912124b9e1cb8dcdaa0780b1ad7be2c3
TDangQuang <95755416+TDangQuang@users.noreply.github.com>
2025-03-07 17:18:17 +0700

    Update DischargePaper.java

--- src/java/Model/DischargePaper.java
+++ src/java/Model/DischargePaper.java
@@ -2,21 +2,25 @@
 package Model;
 
 public class DischargePaper {
-    private String ID, getInDate, getOutDate, treatmentMethod, result, doctorAdvice, diseaseName, departmentName, patientName, employeeName;
+    private String ID, getInDate, getOutDate, treatmentMethod, result, doctorAdvice, diseaseID, diseaseName, departmentID, departmentName, patientID, patientName, employeeID, employeeName;
 
     public DischargePaper() {
     }
 
-    public DischargePaper(String ID, String getInDate, String getOutDate, String treatmentMethod, String result, String doctorAdvice, String diseaseName, String departmentName, String patientName, String employeeName) {
+    public DischargePaper(String ID, String getInDate, String getOutDate, String treatmentMethod, String result, String doctorAdvice, String diseaseID, String diseaseName, String departmentID, String departmentName, String patientID, String patientName, String employeeID, String employeeName) {
         this.ID = ID;
         this.getInDate = getInDate;
         this.getOutDate = getOutDate;
         this.treatmentMethod = treatmentMethod;
         this.result = result;
         this.doctorAdvice = doctorAdvice;
+        this.diseaseID = diseaseID;
         this.diseaseName = diseaseName;
+        this.departmentID = departmentID;
         this.departmentName = departmentName;
+        this.patientID = patientID;
         this.patientName = patientName;
+        this.employeeID = employeeID;
         this.employeeName = employeeName;
     }
 
@@ -60,6 +64,22 @@ public class DischargePaper {
         return employeeName;
     }
 
+    public String getDiseaseID() {
+        return diseaseID;
+    }
+
+    public String getDepartmentID() {
+        return departmentID;
+    }
+
+    public String getPatientID() {
+        return patientID;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
     public void setID(String ID) {
         this.ID = ID;
     }
@@ -100,9 +120,25 @@ public class DischargePaper {
         this.employeeName = employeeName;
     }
 
+    public void setDiseaseID(String diseaseID) {
+        this.diseaseID = diseaseID;
+    }
+
+    public void setDepartmentID(String departmentID) {
+        this.departmentID = departmentID;
+    }
+
+    public void setPatientID(String patientID) {
+        this.patientID = patientID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
     @Override
     public String toString() {
-        return "DischargePaper{" + "ID=" + ID + ", getInDate=" + getInDate + ", getOutDate=" + getOutDate + ", treatmentMethod=" + treatmentMethod + ", result=" + result + ", doctorAdvice=" + doctorAdvice + ", diseaseName=" + diseaseName + ", departmentName=" + departmentName + ", patientName=" + patientName + ", employeeName=" + employeeName + '}';
+        return "DischargePaper{" + "ID=" + ID + ", getInDate=" + getInDate + ", getOutDate=" + getOutDate + ", treatmentMethod=" + treatmentMethod + ", result=" + result + ", doctorAdvice=" + doctorAdvice + ", diseaseID=" + diseaseID + ", diseaseName=" + diseaseName + ", departmentID=" + departmentID + ", departmentName=" + departmentName + ", patientID=" + patientID + ", patientName=" + patientName + ", employeeID=" + employeeID + ", employeeName=" + employeeName + '}';
     }
-    
+
 }
