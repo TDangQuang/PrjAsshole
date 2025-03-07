435320af5fbd6861a46bd9f421e5e39786e898a2
TDangQuang <95755416+TDangQuang@users.noreply.github.com>
2025-03-07 17:05:36 +0700

    Update Employee.java

--- src/java/Model/Employee.java
+++ src/java/Model/Employee.java
@@ -2,16 +2,18 @@
 package Model;
 
 public class Employee {
-    private String ID, name, role, departmentName, majorName;
+    private String ID, name, role, departmentID, majorID, departmentName, majorName;
 
     public Employee() {
     }
 
-    public Employee(String ID, String name, String role, String departmentName, String majorName) {
+    public Employee(String ID, String name, String role, String departmentID, String majorID, String departmentName, String majorName) {
         this.ID = ID;
         this.name = name;
         this.role = role;
+        this.departmentID = departmentID;
         this.departmentName = departmentName;
+        this.majorID = majorID;
         this.majorName = majorName;
     }
 
@@ -35,6 +37,14 @@ public class Employee {
         return majorName;
     }
 
+    public String getDepartmentID() {
+        return departmentID;
+    }
+
+    public String getMajorID() {
+        return majorID;
+    }
+
     public void setID(String ID) {
         this.ID = ID;
     }
@@ -55,9 +65,17 @@ public class Employee {
         this.majorName = majorName;
     }
 
+    public void setDepartmentID(String departmentID) {
+        this.departmentID = departmentID;
+    }
+
+    public void setMajorID(String majorID) {
+        this.majorID = majorID;
+    }
+
     @Override
     public String toString() {
-        return "Employee{" + "ID=" + ID + ", name=" + name + ", role=" + role + ", departmentName=" + departmentName + ", majorName=" + majorName + '}';
+        return "Employee{" + "ID=" + ID + ", name=" + name + ", role=" + role + ", departmentID=" + departmentID + ", departmentName=" + departmentName + ", majorID=" + majorID + ", majorName=" + majorName + '}';
     }
-    
+
 }
