ad639d15ba7a6d53655afbf596f34c4e6a0c6367
TDangQuang <95755416+TDangQuang@users.noreply.github.com>
2025-03-07 17:16:03 +0700

    Update DrugUsageReport.java

--- src/java/Model/DrugUsageReport.java
+++ src/java/Model/DrugUsageReport.java
@@ -2,16 +2,19 @@
 package Model;
 
 public class DrugUsageReport {
-    private String ID, medicineName, patientName, employeeName, useDate;
+    private String ID, medicineID, medicineName, patientID, patientName, employeeID, employeeName, useDate;
     private int quantity;
 
     public DrugUsageReport() {
     }
 
-    public DrugUsageReport(String ID, String medicineName, String patientName, String employeeName, String useDate, int quantity) {
+    public DrugUsageReport(String ID, String medicineID, String medicineName, String patientID, String patientName, String employeeID, String employeeName, String useDate, int quantity) {
         this.ID = ID;
+        this.medicineID = medicineID;
         this.medicineName = medicineName;
+        this.patientID = patientID;
         this.patientName = patientName;
+        this.employeeID = employeeID;
         this.employeeName = employeeName;
         this.useDate = useDate;
         this.quantity = quantity;
@@ -41,6 +44,18 @@ public class DrugUsageReport {
         return quantity;
     }
 
+    public String getMedicineID() {
+        return medicineID;
+    }
+
+    public String getPatientID() {
+        return patientID;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
     public void setID(String ID) {
         this.ID = ID;
     }
@@ -65,9 +80,21 @@ public class DrugUsageReport {
         this.quantity = quantity;
     }
 
+    public void setMedicineID(String medicineID) {
+        this.medicineID = medicineID;
+    }
+
+    public void setPatientID(String patientID) {
+        this.patientID = patientID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
     @Override
     public String toString() {
-        return "DrugUsageReport{" + "ID=" + ID + ", medicineName=" + medicineName + ", patientName=" + patientName + ", employeeName=" + employeeName + ", useDate=" + useDate + ", quantity=" + quantity + '}';
+        return "DrugUsageReport{" + "ID=" + ID + ", medicineID=" + medicineID + ", medicineName=" + medicineName + ", patientID=" + patientID + ", patientName=" + patientName + ", employeeID=" + employeeID + ", employeeName=" + employeeName + ", useDate=" + useDate + ", quantity=" + quantity + '}';
     }
-    
+
 }
