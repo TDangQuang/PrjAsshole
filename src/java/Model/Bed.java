45f8a4c79850b5b7c1d238418ba361ddc520795a
TDangQuang <95755416+TDangQuang@users.noreply.github.com>
2025-03-07 17:07:02 +0700

    Update Bed.java

--- src/java/Model/Bed.java
+++ src/java/Model/Bed.java
@@ -2,14 +2,15 @@
 package Model;
 
 public class Bed {
-    private String ID, room, departmentName;
+    private String ID, room, departmentID, departmentName;
 
     public Bed() {
     }
 
-    public Bed(String ID, String room, String departmentName) {
+    public Bed(String ID, String room, String departmentID, String departmentName) {
         this.ID = ID;
         this.room = room;
+        this.departmentID = departmentID;
         this.departmentName = departmentName;
     }
 
@@ -19,6 +20,10 @@ public class Bed {
 
     public String getRoom() {
         return room;
+    }
+
+    public String getDepartmentID() {
+        return departmentID;
     }
 
     public String getDepartmentName() {
@@ -33,13 +38,17 @@ public class Bed {
         this.room = room;
     }
 
+    public void setDepartmentID(String departmentID) {
+        this.departmentID = departmentID;
+    }
+
     public void setDepartmentName(String departmentName) {
         this.departmentName = departmentName;
     }
 
     @Override
     public String toString() {
-        return "Bed{" + "ID=" + ID + ", room=" + room + ", departmentName=" + departmentName + '}';
+        return "Bed{" + "ID=" + ID + ", room=" + room + ", departmentID=" + departmentID + ", departmentName=" + departmentName + '}';
     }
-    
+
 }
