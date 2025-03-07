921becc6b9f428ca5f4e3a517c4bb959dbe6ce5d
TDangQuang <95755416+TDangQuang@users.noreply.github.com>
2025-03-07 17:08:28 +0700

    Update MedicalRecordBook.java

--- src/java/Model/MedicalRecordBook.java
+++ src/java/Model/MedicalRecordBook.java
@@ -2,18 +2,23 @@
 package Model;
 
 public class MedicalRecordBook {
-    private String ID, medicalRecordName;
+    private String ID, medicalRecordID, medicalRecordName;
 
     public MedicalRecordBook() {
     }
 
-    public MedicalRecordBook(String ID, String medicalRecordName) {
+    public MedicalRecordBook(String ID, String medicalRecordID, String medicalRecordName) {
         this.ID = ID;
+        this.medicalRecordID = medicalRecordID;
         this.medicalRecordName = medicalRecordName;
     }
 
     public String getID() {
         return ID;
+    }
+
+    public String getMedicalRecordID() {
+        return medicalRecordID;
     }
 
     public String getMedicalRecordName() {
@@ -24,13 +29,17 @@ public class MedicalRecordBook {
         this.ID = ID;
     }
 
+    public void setMedicalRecordID(String medicalRecordID) {
+        this.medicalRecordID = medicalRecordID;
+    }
+
     public void setMedicalRecordName(String medicalRecordName) {
         this.medicalRecordName = medicalRecordName;
     }
 
     @Override
     public String toString() {
-        return "MedicalRecordBook{" + "ID=" + ID + ", medicalRecordName=" + medicalRecordName + '}';
+        return "MedicalRecordBook{" + "ID=" + ID + ", medicalRecordID=" + medicalRecordID + ", medicalRecordName=" + medicalRecordName + '}';
     }
-    
+
 }
