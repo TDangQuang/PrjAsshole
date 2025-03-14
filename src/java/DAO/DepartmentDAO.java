50d6d85e97d9b2bbf17e2371a6d78e6a62cc4fa7
TDangQuang <95755416+TDangQuang@users.noreply.github.com>
2025-03-14 16:39:52 +0700

    Update DepartmentDAO.java

--- src/java/DAO/DepartmentDAO.java
+++ src/java/DAO/DepartmentDAO.java
@@ -2,7 +2,6 @@
 package DAO;
 
 import Model.Department;
-import Model.Patient;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -27,7 +26,9 @@ public class DepartmentDAO {
             while (rs.next()) {
                 String ID = rs.getString(1);
                 String name = rs.getString(2);
-                departmentList.add(new Department(ID, name));
+                String description = rs.getString(3);
+                String location = rs.getString(4);
+                departmentList.add(new Department(ID, name, description, location));
             }
             conn.close();
         } catch (Exception ex) {
@@ -46,7 +47,9 @@ public class DepartmentDAO {
             rs = ps.executeQuery();
             if (rs.next()) {
                 String name = rs.getString(2);
-                return new Department(ID, name);
+                String description = rs.getString(3);
+                String location = rs.getString(4);
+                return new Department(ID, name, description, location);
             }
         } catch (Exception ex) {
             Logger.getLogger(DepartmentDAO.class.getName()).log(Level.SEVERE, null, ex);
@@ -71,15 +74,17 @@ public class DepartmentDAO {
         return "D001";
     }
 
-    public void addDepartment(String name) {
+    public void addDepartment(String name, String description, String location) {
         String ID = getIDForNewDepartment();
         DBContext db = new DBContext();
         try {
             conn = db.getConnection();
-            String sql = "INSERT INTO Department VALUES(?, ?)";
+            String sql = "INSERT INTO Department VALUES(?, ?, ?, ?)";
             ps = conn.prepareStatement(sql);
             ps.setString(1, ID);
             ps.setString(2, name);
+            ps.setString(3, description);
+            ps.setString(4, location);
             ps.executeUpdate();
             conn.close();
         } catch (Exception ex) {
@@ -101,20 +106,44 @@ public class DepartmentDAO {
         }
     }
 
-    public void updateDepartment(String ID, String name) {
+    public void updateDepartment(String ID, String name, String description, String location) {
         DBContext db = new DBContext();
         try {
             conn = db.getConnection();
             String sql = """
                          UPDATE Department
-                         SET [name] = ? WHERE ID = ?""";
+                         SET [name] = ?, [description] = ?, [location] = ? WHERE ID = ?""";
             ps = conn.prepareStatement(sql);
             ps.setString(1, name);
-            ps.setString(2, ID);
+            ps.setString(2, description);
+            ps.setString(3, location);
+            ps.setString(4, ID);
             ps.executeUpdate();
             conn.close();
         } catch (Exception ex) {
             Logger.getLogger(DepartmentDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    public List<Department> getAllDepartmentSearchByName(String searchName) {
+        DBContext db = new DBContext();
+        List<Department> departmentList = new ArrayList();
+        try {
+            conn = db.getConnection();
+            String sql = "SELECT * FROM Department WHERE Department.name LIKE '% " + searchName + "%'";
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                String ID = rs.getString(1);
+                String name = rs.getString(2);
+                String description = rs.getString(3);
+                String location = rs.getString(4);
+                departmentList.add(new Department(ID, name, description, location));
+            }
+            conn.close();
+        } catch (Exception ex) {
+            Logger.getLogger(DepartmentDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return departmentList;
     }
 }
